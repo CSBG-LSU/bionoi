7717c78c255210a27dfc9dee1d45748b99d896b5
yeah <x@gmail.com>
2018-10-15 17:05:08 -0400

    update CLI args and documents

--- README.md
+++ README.md
@@ -1,44 +1,38 @@
 # bionoi
 Conversion of biomolecules to Voronoi diagrams.
 
-## Description 
-bionoi constructs the [voronoi diagram](https://en.wikipedia.org/wiki/Voronoi_diagram) (VD) of protein binding site/pocket or ligand structure based on the 3D or 2D coordinate structure. 
-                                                         
+## Description
+bionoi constructs the [voronoi diagram](https://en.wikipedia.org/wiki/Voronoi_diagram) (VD) of protein binding site/pocket or ligand structure based on the 3D or 2D coordinate structure.
+
 ## Requirements
-1. Python 2.7+
+1. Python 3
 2. numpy 1.14+
 3. scipy 0.18+
 4. Pandas 0.19+
 5. scikit-spatial 0.12.0
 6. matplotlib 2.0.2+
-7. biopandas ; eg : pip/conda install biopandas  
+7. biopandas ; eg : pip/conda install biopandas
 
 ## Getting started
 
 1. copy/download the code from GitHub
-2. If input is a 3D coordinate of protein/ligand, it will be projected to 2D plane 
+2. If input is a 3D coordinate of protein/ligand, it will be projected to 2D plane
 3. Run voronoi.py with a .mol2 file (see Examples)
 
 ## Examples
 
 create a 2D image using mol2 file in 2D or 3D format
 
-    python deepdrugV.py -mol input.mol2 -out output.jpg -dpi integer 
-    
-    -mol stands for the receptor, protein or ligand (MUST BE in a mol2 format)
-    -out is the name of the output file [OPTION](default is jpg format)
-    -dpi is the desired image quality [OPTION](default 120 dpi in 2.7 x 2.7 = 256 x 256 px)
+    ./voronoi.py
+    or
+    ./voronoi.py -mol a.mol2 -out a.jpg -dpi 120 -alpha 0.5
 
-creating a 2D image of voronoi using protein pocket file in mol2 3D format
+An example of voronoi image of ATP-binding site protein pocket colored by atom types:
 
-    python deepdrugV.py -mol 4v94E.mol2 -out Voronoi_2D_4v94E.jpg -dpi 120   
-    
-Voronoi image of ATP-binding site protein pocket colored by atom types:
- 
-![eg_image](https://github.com/rajiv03/DeepDrugV/blob/master/Voronoi_2D_4v94E.jpg) 
+![eg_image](https://github.com/rajiv03/DeepDrugV/blob/master/Voronoi_2D_4v94E.jpg)
 
 chaperonin (4v94, chain E)
 
 Contributors:
 
-Rajiv Gandhi Govindaraj, Jeffrey Lemoine, Limeng Pu, and Michal Brylinski. 
+Rajiv Gandhi Govindaraj, Jeffrey Lemoine, Limeng Pu, Ye Fang and Michal Brylinski.
--- voronoi.py
+++ voronoi.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np
 import pandas as pd
@@ -5,6 +7,9 @@ import matplotlib
 import sys,os,argparse
 from biopandas.mol2 import PandasMol2
 import matplotlib.pyplot as plt
+
+# Performance tweaks
+#import cProfile
 
 def voronoi_finite_polygons_2d(vor, radius=None):
     """
@@ -26,10 +31,10 @@ def voronoi_finite_polygons_2d(vor, radius=None):
         Coordinates for revised Voronoi vertices. Same as coordinates
         of input vertices, with 'points at infinity' appended to the
         end.
-        
+
     Source
     -------
-    Copied from https://gist.github.com/pv/8036995 
+    Copied from https://gist.github.com/pv/8036995
     """
 
     if vor.points.shape[1] != 2:
@@ -94,39 +99,39 @@ def voronoi_finite_polygons_2d(vor, radius=None):
 
 def fig_to_numpy(fig, alpha=1) -> np.ndarray:
     '''
-    Converts matplotlib figure to a numpy array. 
-    
-    Source 
+    Converts matplotlib figure to a numpy array.
+
+    Source
     ------
     Adapted from https://stackoverflow.com/questions/7821518/matplotlib-save-plot-to-numpy-array
     '''
-    
-    # Setup figure 
-    fig.patch.set_alpha(alpha)    
+
+    # Setup figure
+    fig.patch.set_alpha(alpha)
     fig.canvas.draw()
 
     # Now we can save it to a numpy array.
     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    
+
     return data
 
-def voronoi_atoms(bs,cmap, bs_out=None,size=None, alpha=0.5, save_fig=True, projection=lambda a,b: a/abs(b)**.5):
+def voronoi_atoms(bs, cmap, bs_out=None, size=None, alpha=0.5, save_fig=True, projection=lambda a,b: a/abs(b)**.5):
     # Suppresses warning
     pd.options.mode.chained_assignment = None
-    
-    # Read molecules in mol2 format 
+
+    # Read molecules in mol2 format
     mol2 = PandasMol2().read_mol2(bs)
-    atoms = mol2.df[['subst_name','atom_type', 'atom_name','x','y','z']] 
-   
+    atoms = mol2.df[['subst_name','atom_type', 'atom_name','x','y','z']]
+
     # Todo
-    # Align atoms to principal axis and save eigenvalue. 
+    # Align atoms to principal axis and save eigenvalue.
     # See issue #2
 
-    # convert 3D  to 2D 
-    atoms["P(x)"] = atoms[['x','y','z']].apply(lambda coord: projection(coord.x,coord.z), axis=1) 
-    atoms["P(y)"] = atoms[['x','y','z']].apply(lambda coord: projection(coord.y,coord.z), axis=1)  
-    
+    # convert 3D  to 2D
+    atoms["P(x)"] = atoms[['x','y','z']].apply(lambda coord: projection(coord.x,coord.z), axis=1)
+    atoms["P(y)"] = atoms[['x','y','z']].apply(lambda coord: projection(coord.y,coord.z), axis=1)
+
     # setting output image size, labels off, set 128 dpi w x h
     size = 128 if size is None else size
     figure = plt.figure(figsize=(6 , 6),dpi=int(size))
@@ -142,67 +147,70 @@ def voronoi_atoms(bs,cmap, bs_out=None,size=None, alpha=0.5, save_fig=True, proj
         polygon = vertices[reg]
         polygons.append(polygon)
     atoms.loc[:,'polygons'] = polygons
-        
+
     # Check alpha
     alpha=float(alpha)
-        
+
     colors = []
     for i, row in atoms.iterrows():
         atom_type = atoms.loc[i][['atom_type']][0]
         color = cmap[atom_type]["color"]
         colors.append(color)
-        colored_cell = matplotlib.patches.Polygon(row["polygons"],  
-                                        facecolor = color, 
+        colored_cell = matplotlib.patches.Polygon(row["polygons"],
+                                        facecolor = color,
                                         edgecolor = 'black',
                                         alpha = alpha  )
         ax.add_patch(colored_cell)
     atoms.loc[:,"color"] = colors
-     
+
     ax.set_xlim(vor.min_bound[0] , vor.max_bound[0])
     ax.set_ylim(vor.min_bound[1] , vor.max_bound[1] )
 
     # Output image saving in any format; default jpg
     bs_out = 'out.jpg' if bs_out is None else bs_out
-    
+
     # Get image as numpy array
     figure.tight_layout(pad=0)
     img = fig_to_numpy(figure, alpha=alpha)
-    
-    if save_fig: 
+
+    if save_fig:
         plt.savefig(bs_out, frameon=False,bbox_inches="tight", pad_inches=False)
-    
+
     return atoms, vor, img
 
 def getArgs():
-    parser = argparse.ArgumentParser('python')                                              
-    parser.add_argument('-mol', required = True, help = 
-                        'location of the protein/ligand mol2 file path')
-    parser.add_argument('-cmap', required = False, help = 
-                        'location of the cmap file. \'./labels_mol2.csv\' is default. ')
-    parser.add_argument('-out', required = False, help = 'location for the image to be saved')
-    parser.add_argument('-dpi', required = False, help = 'image quality in dpi, eg: 300')
-    parser.add_argument('-alpha', required = False, help = 'alpha for color of cells')
-    
+    parser = argparse.ArgumentParser('python')
+    parser.add_argument('-mol',   default="./a.mol2",          required = False, help = 'the protein/ligand mol2 file')
+    parser.add_argument('-cmap',  default="./labels_mol2.csv", required = False, help = 'the cmap file. ')
+    parser.add_argument('-out',   default="./a.jpg",           required = False, help = 'the outpuot image file')
+    parser.add_argument('-dpi',   default="120",               required = False, help = 'image quality in dpi')
+    parser.add_argument('-alpha', default="0.5",               required = False, help = 'alpha for color of cells')
+
     return parser.parse_args()
 
-def Bionoi(mol, cmap_fpath="./labels_mol2.csv", bs_out='out.jpg', size=128, alpha=0.5):
-    # Check for color mapping file, make dict 
+def Bionoi(mol, cmap, bs_out, dpi, alpha):
+    # Check for color mapping file, make dict
     try:
-        with open(cmap_fpath) as cMapF:
-            # Parse color map file 
+        with open(cmap) as cMapF:
+            # Parse color map file
             cmap  = np.array([line.replace("\n","").split("; ") for line in cMapF.readlines() if not line.startswith("#")])
             # To dict
             cmap = {atom:{"color":color, "definition":definition} for atom, definition, color in cmap}
-            
+
+
     except FileNotFoundError:
         raise FileNotFoundError("Color mapping file not found in directory")
- 
-    # Run 
-    atoms, vor, img = voronoi_atoms(mol,cmap, bs_out=bs_out, size=size, alpha=alpha, save_fig=True)
-    
+
+    # Run
+    atoms, vor, img = voronoi_atoms(mol, cmap, bs_out=bs_out, size=dpi, alpha=alpha, save_fig=True)
+
     return atoms, vor, img
+
+
 
 if __name__ == "__main__":
     args = getArgs()
-    atoms, vor, img = Bionoi(args.mol, bs_out=args.out,size=args.dpi, alpha=args.alpha)
-    
+    atoms, vor, img = Bionoi(args.mol, args.cmap, args.out, args.dpi, args.alpha)
+    #atoms, vor, img = cProfile.run('Bionoi(args.mol, args.cmap, args.out, args.dpi, args.alpha)')
+
+
